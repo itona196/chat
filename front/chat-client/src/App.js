bac887baafa2d7ebb9ba37e51d779e8b739da393
itona <esteb.sol@bluewin.ch>
2025-05-14 13:54:11 +0200

    modif

--- front/chat-client/src/App.js
+++ front/chat-client/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://192.168.101.21:3000");
+const socket = io("https://chat-6qhm.onrender.com");
 
 function App() {
   const [msg, setMsg] = useState("");
