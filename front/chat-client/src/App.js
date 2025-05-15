09b9a8f93498c395de07f829e2609a8ff640b922
Your Name <you@example.com>
2025-05-15 07:43:25 +0200

    force: rebuild with correct env var

--- front/chat-client/src/App.js
+++ front/chat-client/src/App.js
@@ -3,7 +3,9 @@ import io from "socket.io-client";
 import "./App.css";
 
 console.log("SOCKET_URL =", process.env.REACT_APP_SOCKET_URL);
+
 const socket = io(process.env.REACT_APP_SOCKET_URL);
+
 
 function App() {
   const [msg, setMsg] = useState("");
